71f5d43c2aca5bbd258fa49b2c9ab4bb76e0918a
CristhianMotoche <cristhian.motoche@gmail.com>
2021-03-08 17:32:34 -0500

    Render simple rectangle

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello world");
+const CANVAS_ID = "game";
+
+const main = (): void => {
+  const canvas = document.getElementById(CANVAS_ID) as HTMLCanvasElement;;
+  const ctx = canvas.getContext("2d");
+
+  if (ctx) {
+    draw(ctx);
+  } else {
+    alert("Invalid context")
+  }
+};
+
+const draw = (ctx: CanvasRenderingContext2D): void => {
+  ctx.fillStyle = "#f00";
+  ctx.fillRect(20, 20, 100, 100);
+}
+
+main();
