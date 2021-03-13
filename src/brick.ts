15bd8905087df30977dbfd6ed86949880f471f8a
CristhianMotoche <cristhian.motoche@gmail.com>
2021-03-13 15:27:47 -0500

    Add brick

---
+++ src/brick.ts
@@ -0,0 +1,22 @@
+import Position from "./position";
+
+export default class Brick {
+  width: number;
+  height: number;
+  position: Position;
+
+  constructor(position: Position) {
+    this.width = 50;
+    this.height = 30;
+    this.position = position;
+  }
+
+  update(deltaTime: number) {
+
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.fillStyle = "#0f0";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,7 @@
 import Paddle from "./paddle";
 import InputHandler from "./input";
 import Ball from "./ball";
+import Brick from "./brick";
 
 
 export default class Game {
@@ -8,12 +9,17 @@ export default class Game {
   ball: Ball;
   gameWidth: number;
   gameHeight: number;
+  bricks: Brick[];
 
   constructor(gameWidth: number, gameHeight: number) {
     this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
     this.paddle = new Paddle(this);
     this.ball = new Ball(this);
+    this.bricks = [];
+    for (let i = 0; i < 7; i++){
+      this.bricks.push(new Brick({ x: i*80 + 5, y: 20 }));
+    }
   }
 
   start() {
@@ -28,5 +34,6 @@ export default class Game {
   draw(ctx: CanvasRenderingContext2D) {
     this.paddle.draw(ctx);
     this.ball.draw(ctx);
+    this.bricks.forEach(brick => brick.draw(ctx));
   }
 }
