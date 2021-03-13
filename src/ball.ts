3d1db71fb5d7a7d898a05919573f375bf9664d8f
CristhianMotoche <cristhian.motoche@gmail.com>
2021-03-13 15:08:37 -0500

    Collision between ball and paddle

--- src/ball.ts
+++ src/ball.ts
@@ -2,15 +2,13 @@ import Position from "./position";
 import Game from "./game";
 
 export default class Ball {
+  game: Game;
   position: Position;
   speed: Position;
   size: number;
-  gameWidth: number;
-  gameHeight: number;
 
   constructor(game: Game) {
-    this.gameHeight = game.gameHeight;
-    this.gameWidth = game.gameWidth;
+    this.game = game;
     this.size = 10;
     this.position = {
       x: 200,
@@ -39,9 +37,20 @@ export default class Ball {
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
 
-    if(this.position.x > this.gameWidth || this.position.x < 0)
+    // Hit on left and right
+    if(this.position.x > this.game.gameWidth || this.position.x < 0)
       this.speed.x = -this.speed.x;
-    if(this.position.y > this.gameHeight || this.position.y < 0)
+    // Hit on top and bottom
+    if(this.position.y > this.game.gameHeight || this.position.y < 0)
       this.speed.y = -this.speed.y;
+    // Hit on paddle
+    const bottomOfBall = this.position.y + this.size;
+    const topOfPaddle =  this.game.paddle.position.y;
+    const leftOfPaddle = this.game.paddle.position.x;
+    const rightOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
+
+    if (bottomOfBall >= topOfPaddle && (this.position.x >= leftOfPaddle && this.position.x <= rightOfPaddle)) {
+      this.speed.y = -this.speed.y;
+    }
   }
 }
