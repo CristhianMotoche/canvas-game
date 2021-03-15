abc400cc0eb96dcdab44443a1ffbf098040afe52
CristhianMotoche <cristhian.motoche@gmail.com>
2021-03-15 17:36:39 -0500

    Be able to pause the game

--- src/game.ts
+++ src/game.ts
@@ -5,14 +5,21 @@ import Brick from "./brick";
 import { Level, buildLevel } from "./levels";
 
 
+enum GameState {
+  PAUSE,
+  PLAY,
+}
+
 export default class Game {
   paddle: Paddle;
   ball: Ball;
   gameWidth: number;
   gameHeight: number;
   bricks: Brick[];
+  state: GameState;
 
   constructor(gameWidth: number, gameHeight: number, level: Level) {
+    this.state = GameState.PLAY;
     this.gameWidth = gameWidth;
     this.gameHeight = gameHeight;
     this.paddle = new Paddle(this);
@@ -21,10 +28,12 @@ export default class Game {
   }
 
   start() {
-    new InputHandler(this.paddle);
+    new InputHandler(this.paddle, this);
   }
 
   update(deltaTime: number) {
+    if (this.state == GameState.PAUSE) return;
+
     this.paddle.update(deltaTime);
     this.ball.update(deltaTime);
     this.bricks.forEach(brick => brick.update(deltaTime));
@@ -32,8 +41,21 @@ export default class Game {
   }
 
   draw(ctx: CanvasRenderingContext2D) {
+    if (this.state == GameState.PAUSE) {
+      ctx.fillStyle = "#aaa";
+      ctx.fillRect(0, 0, this.gameWidth, this.gameHeight);
+    }
+
     this.paddle.draw(ctx);
     this.ball.draw(ctx);
     this.bricks.forEach(brick => brick.draw(ctx));
+  }
+
+  togglePause() {
+    if (this.state == GameState.PAUSE) {
+      this.state = GameState.PLAY;
+    } else {
+      this.state = GameState.PAUSE;
+    }
   }
 }
--- src/input.ts
+++ src/input.ts
@@ -1,9 +1,10 @@
 import Paddle from "./paddle";
+import Game from "./game";
 
-const LEFT_ARROW = 37, RIGHT_ARROW = 39;
+const LEFT_ARROW = 37, RIGHT_ARROW = 39, ESC = 27;
 
 export default class InputHandler {
-  constructor(paddle: Paddle) {
+  constructor(paddle: Paddle, game: Game) {
     document.addEventListener("keydown", event => {
       switch (event.keyCode) {
         case LEFT_ARROW:
@@ -11,6 +12,9 @@ export default class InputHandler {
           break;
         case RIGHT_ARROW:
           paddle.moveRight();
+          break;
+        case ESC:
+          game.togglePause();
           break;
       }
     })
